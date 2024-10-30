6b9df238cd332376cedda7b0b4d1e96b20270f8d
scamassa <sara.camassa@heig-vd.ch>
2024-10-30 15:53:39 +0100

    tests and comments

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,12 +1,24 @@
 import java.lang.Math;
 
+/*
+ * Classe représentant une matrice de taille quelconque M x N contenant des éléments entre 0 et modulo-1
+ * @author David Berger, Sara Camassa
+ */
 public class Matrix {
     private int m, n, modulo;
     private int[][] matrix;
 
     public Matrix(int m, int n, int mod) {
-        this.m = m;
-        this.n = n;
+        if(m < 0 || n < 0) {
+            throw new RuntimeException("Invalid matrix size");
+        } else if(m == 0 || n == 0) {
+            this.m = 0;
+            this.n = 0;
+        } else {
+            this.m = m;
+            this.n = n;
+        }
+
         this.matrix = new int[m][n];
         this.modulo = mod;
 
@@ -18,19 +30,30 @@ public class Matrix {
     }
 
     public Matrix(int[][] values, int mod) {
-        this.m = values.length;
-        this.n = values[0].length;
+        if (values.length == 0 || values[0].length == 0) {
+            this.m = 0;
+            this.n = 0;
+        } else {
+            this.m = values.length;
+            this.n = values[0].length;
+        }
         this.matrix = new int[m][n];
         this.modulo = mod;
 
-
         for(int i = 0; i < this.m; i++) {
+            if(values[i] == null) throw new RuntimeException("Invalid matrix element (null)");
             for(int j = 0; j < this.n; j++) {
                 matrix[i][j] = Math.floorMod(values[i][j], this.modulo);
             }
         }
     }
 
+    /*
+     * Fonction effectuant l'opération souhaitée entre deux matrices.
+     * @param m1 la première matrice
+     * @param operation la MatrixOperation que l'on souhaite appliquer
+     * @param m2 la deuxième matrice
+     */
     public static Matrix matrixOperation(Matrix m1, MatrixOperation operation, Matrix m2) {
         if (m1.modulo != m2.modulo) {
             throw new RuntimeException("mods are not equal");
--- src/Test.java
+++ src/Test.java
@@ -1,12 +1,28 @@
+
+/*
+ * Contient des fonctions de tests sur la classe Matrix et les opérations MatrixOperation.
+ * @author David Berger, Sara Camassa
+ */
 public class Test {
 
+    private static Matrix testAddMatrix(Matrix m1, Matrix m2) {
+        return Matrix.matrixOperation(m1, new MatrixAddition(), m2);
+    }
+
+    private static Matrix testSubMatrix(Matrix m1, Matrix m2) {
+        return Matrix.matrixOperation(m1, new MatrixAddition(), m2);
+    }
+
+    private static Matrix testMultMatrix(Matrix m1, Matrix m2) {
+        return Matrix.matrixOperation(m1, new MatrixAddition(), m2);
+    }
+
     /*
-    * modulo negatif
-    * grands chiffres
-    * modulo differents
-    * taille 0
-    * taille negative
-    * */
+     * Teste la création de matrices (éléments aléatoires) et les opérations sur leurs éléments.
+     * @param M1, N1 la taille de la première matrice
+     * @param m2, N2 la taille de la deuxième matrice
+     * @param mod le modulo des deux matrices
+     */
     public static void testMatrix(int M1, int N1, int M2, int N2, int mod) {
         Matrix m1 = new Matrix(M1, N1, mod);
         Matrix m2 = new Matrix(M2, N2, mod);
@@ -21,15 +37,170 @@ public class Test {
         System.out.println("one x two\n" + testMultMatrix(m1, m2));
     }
 
-    private static Matrix testAddMatrix(Matrix m1, Matrix m2) {
-        return Matrix.matrixOperation(m1, new MatrixAddition(), m2);
+    /*
+     * Teste la création de matrices (éléments choisis) et les opérations sur leurs éléments.
+     * @param val1 le tableau 2d de int des valeurs de la première matrice
+     * @param val2 le tableau 2d de int des valeurs de la deuxième matrice
+     * @param mod1, mod2 les modulos des deux matrices respectivement
+     */
+    public static void testMatrixGivenElements(int[][] val1, int [][] val2, int mod1, int mod2) {
+        Matrix m1 = new Matrix(val1, mod1);
+        Matrix m2 = new Matrix(val2, mod2);
+
+        testAddMatrix(m1, m2);
+        testSubMatrix(m1, m2);
+        testMultMatrix(m1, m2);
+
+        System.out.println("passed");
     }
 
-    private static Matrix testSubMatrix(Matrix m1, Matrix m2) {
-        return Matrix.matrixOperation(m1, new MatrixAddition(), m2);
+    /*
+     * Fonction identique à testMatrix(), mais affiche uniquement le mot "passed" dans le cas où aucune erreur n'est survenue.
+     */
+    public static void testMatrixNoComments(int M1, int N1, int M2, int N2, int mod) {
+        Matrix m1 = new Matrix(M1, N1, mod);
+        Matrix m2 = new Matrix(M2, N2, mod);
+
+        testAddMatrix(m1, m2);
+        testSubMatrix(m1, m2);
+        testMultMatrix(m1, m2);
+
+        System.out.println("passed");
     }
 
-    private static Matrix testMultMatrix(Matrix m1, Matrix m2) {
-        return Matrix.matrixOperation(m1, new MatrixAddition(), m2);
+    /*
+     * Teste la création de matrices (éléments aléatoires) et les opérations sur leurs éléments.
+     * @param M1, N1 la taille de la première matrice
+     * @param m2, N2 la taille de la deuxième matrice
+     * @param mod1, mod2 le modulo des deux matrices respectivement
+     */
+    public static void testDiffModMatrix(int M1, int N1, int M2, int N2, int mod1, int mod2) {
+        Matrix m1 = new Matrix(M1, N1, mod1);
+        Matrix m2 = new Matrix(M2, N2, mod2);
+
+        testAddMatrix(m1, m2);
+        testSubMatrix(m1, m2);
+        testMultMatrix(m1, m2);
+
+        System.out.println("-> passed");
+    }
+
+    public static void main(String[] args) {
+        int[][] val1 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}};
+        int[][] val2 = {{1, 2}, {3, 4}, {5, 6}};
+        int[][] valMaxInt = {{-2147483648, 2147483647}, {3, 4}, {5, 6}};
+        int[][] valNullRow = {{1, 2}, null, {3, 4}, {5, 6}};
+        int[][] valSize0 = new int[0][0];
+        int[][] valSize1 = new int[1][1];
+
+        System.out.println("Tests of Matrix class and operations: random elements");
+
+        System.out.println("same sizes:");
+        try {
+            testMatrixNoComments(5, 4, 5, 4, 6);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        System.out.println("different sizes:");
+        try {
+            testMatrixNoComments(5, 4, 6, 3, 6);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        System.out.println("Different modulus:");
+        try {
+            testDiffModMatrix(5, 4, 6, 3, 7, 6);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        System.out.println("Negative modulus:");
+        try {
+            testMatrixNoComments(5, 4, 6, 3, -2);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        System.out.println("negative size:");
+        try {
+            testMatrixNoComments(-3, 4, 6, 3, 7);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        System.out.println("size 0:");
+        try {
+            testMatrixNoComments(0, 4, 6, 3, 7);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        System.out.println("size 1:");
+        try {
+            testMatrixNoComments(1, 1, 6, 3, 7);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        System.out.println("\nTests of Matrix class and operations: given elements");
+
+        System.out.println("same sizes:");
+        try {
+            testMatrixGivenElements(val1, val1, 7, 7);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        System.out.println("different sizes:");
+        try {
+            testMatrixGivenElements(val1, val2, 7, 7);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        System.out.println("Different modulus:");
+        try {
+            testMatrixGivenElements(val1, val2, 7, 3);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        System.out.println("Negative modulus:");
+        try {
+            testMatrixGivenElements(val1, val2, -7, -7);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        System.out.println("size 0:");
+        try {
+            testMatrixGivenElements(valSize0, val2, 2, 2);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        System.out.println("size 1:");
+        try {
+            testMatrixGivenElements(valSize1, val2, 2, 2);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        System.out.println("one row is null:");
+        try {
+            testMatrixGivenElements(valNullRow, val2, 2, 2);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        System.out.println("element is int limit:");
+        try {
+            testMatrixGivenElements(valMaxInt, val2, 2, 2);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
     }
 }
